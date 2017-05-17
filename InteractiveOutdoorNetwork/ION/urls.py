ca5986e99245f1b327289bb6f9b440948f314787
verycumbersome <matthewsages@gmail.com>
2017-05-16 17:38:24 -0700

    Add login page

--- InteractiveOutdoorNetwork/ION/urls.py
+++ InteractiveOutdoorNetwork/ION/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+# from django.contrib.auth import views as auth_views
+from django.contrib import admin
 from ION import views
 
 urlpatterns = [
     url(r'^$', views.Main.as_view(), name='home'),
     url(r'^timeline/', views.timeline, name='timeline'),
-    url(r'^login/', views.login, name='login'),
+    # url(r'^accounts/login/$', auth_views.login, name='login'),
     url(r'^signup', views.signup, name='signup'),
     url(r'^blog/', views.blog, name='blog'),
     url(r'^geartrade/buy', views.geartrade, name='gearbuy'),
--- InteractiveOutdoorNetwork/InteractiveOutdoorNetwork/urls.py
+++ InteractiveOutdoorNetwork/InteractiveOutdoorNetwork/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/login/$', auth_views.login, {'template_name': 'registration/login.html'}),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('ION.urls', namespace='ION')),
 ]
