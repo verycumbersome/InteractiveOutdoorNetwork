ca76b3d1d0ffaa5371e10d570816dedf933d4e14
verycumbersome <matthewsages@gmail.com>
2017-05-28 12:47:04 -0700

    Add password reset and user instance to gearpost model

--- InteractiveOutdoorNetwork/ION/models.py
+++ InteractiveOutdoorNetwork/ION/models.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django_markdown.models import MarkdownField
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 import uuid
 import firebasemanager
 
@@ -24,7 +26,7 @@ class BlogPost(models.Model):
         return self.title
 
 class GearPost(models.Model):
-    author = models.ForeignKey('auth.User')
+    user = models.ForeignKey(User)
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=1000)
     photo = models.ImageField(upload_to='gearimages/', default='/ION/static/images/imgnotfound.png')
