fa4722cfa827d925f28f9aa8ea22401ea4082272
verycumbersome <matthewsages@gmail.com>
2017-09-13 20:21:06 -0700

    Add heroku to allowed hosts

--- InteractiveOutdoorNetwork/settings.py
+++ InteractiveOutdoorNetwork/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = '=a1*yv%k2h)^sjyv7vg)+#$iuqf+dupl(3af=lq4y=7-w4qf#j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'verycumbersome.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = [u'verycumbersome.pythonanywhere.com',
+                'interactive-outdoor-program.herokuapp.com',
+                '127.0.0.1',
+                'localhost',
+                ]
 
 # Application definition
 
