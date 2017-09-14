dcf3b7f162f9bcf561fddafd711faf8beef65a64
verycumbersome <matthewsages@gmail.com>
2017-09-13 20:12:49 -0700

    Deploy to Heroku

--- InteractiveOutdoorNetwork/settings.py
+++ InteractiveOutdoorNetwork/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn InteractiveOutdoorNetwork.wsgi --log-file -
